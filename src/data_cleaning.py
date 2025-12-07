f55356d6ae983d62f1c9b87aaac1b8a312dedb63
DamarisMaldonadoM <damarism@usf.edu>
2025-12-06 22:37:38 -0500

    Load sales_data_raw.csv into data cleaning

---
+++ src/data_cleaning.py
@@ -0,0 +1,6 @@
+#The data cleaning
+import pandas as pd
+
+#Load sales_data_raw.csv from data/raw/
+df = pd.read_csv('data/raw/sales_data_raw.csv') #makes the script able to read the csv
+#Also helps for the cleaning
